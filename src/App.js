1f3ebce976c69f596245ef25a0c6a1da116130c8
xiyu-526 <997578871@qq.com>
2020-06-17 21:30:32 +0800

    提交

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: xiyu
  * @Date: 2020-04-13 13:29:50 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2020-06-17 21:24:42
+ * @Last Modified time: 2020-06-17 21:30:25
  */
 
 import React from 'react';
@@ -53,28 +53,59 @@ function App() {
       <Temp a="你" b="无敌" c="牛逼日飞机"></Temp>
       <h2>React测试项目<br />哔哩哔哩网站数据列表爬取</h2>
       <Pip pipe={mode=>pipe(mode)} />
-      <ul>
-            
-            <li>
-            <Prompt message={tsEvents} />
-            <Link to={{
-              pathname: '/courses',
-              search: '?sort=name',
-              hash: '#the-hash',
-              state: { fromDashboard: true }
-            }}>带参数的链接</Link>
-          </li>
-          <li><Link to="/fliter">滤镜调试中</Link> </li>
-          <li><Link to="/balls">球球</Link> </li>
-          <li><Link to="/tanchishe">贪吃蛇</Link> </li>
-          <li><Link to="/slider">slider</Link> </li>
-          <li><Link to="/list">二级目录</Link> </li>
-          <li><Link to="/nested/one">免费征途</Link></li>
-          <li><Link to="/nested/two">征途2</Link></li>
-          <li><Link replace to="/nested/Three">征途2手游</Link></li>
-          
-        </ul>
-      
+      <div className="App">
+         <header className="App-header">
+            <nav>
+              <NavLink exact activeClassName="active" to="/">Home</NavLink>
+              <NavLink activeStyle={{fontSize:'30px',color: '#ff0000'}} to="/content">Content</NavLink>
+              <NavLink isActive={isActiveFunc} activeClassName="active"  to="/list">List</NavLink>
+            </nav>
+            <ul>
+                
+                <li>
+                <Prompt message={tsEvents} />
+                <Link to={{
+                  pathname: '/courses',
+                  search: '?sort=name',
+                  hash: '#the-hash',
+                  state: { fromDashboard: true }
+                }}>带参数的链接</Link>
+              </li>
+              <li><Link to="/fliter">滤镜调试中</Link> </li>
+              <li><Link to="/balls">球球</Link> </li>
+              <li><Link to="/tanchishe">贪吃蛇</Link> </li>
+              <li><Link to="/slider">slider</Link> </li>
+              <li><Link to="/list">二级目录</Link> </li>
+              <li><Link to="/nested/one">免费征途</Link></li>
+              <li><Link to="/nested/two">征途2</Link></li>
+              <li><Link replace to="/nested/Three">征途2手游</Link></li>
+              
+            </ul>
+            <Switch>
+               <Route path="/fliter" component={Filter}></Route>
+               <Route path="/balls" component={Balls}></Route>
+               <Route path="/tanchishe" render={()=>(
+                  <Tcs>
+                    <Route path="/list" component={List} />
+                  </Tcs>
+                )}></Route>
+                <Route path="/slider" component={Slider}></Route>
+                <Route path="/content" component={Content}></Route>
+                <Route path="/list" render={()=>(
+                    <List ary={keys}>
+                        <Route path="/list/slider" component={Slider} />
+                    </List>
+                )}>
+                </Route>
+                <Route path="/" render={
+                  ()=>(
+                    <Redirect to="/List" />
+                  )}>
+                </Route>
+
+              </Switch>
+        </header>
+       </div>
     </Router>
   );
   // return (
